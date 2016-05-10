da04915f85a4b774afabcf0879f23db715f12ba0
Taras Pashko <taras.pashko@gmail.com>
2016-05-10 13:12:50 +0000

    mm

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 /*global $*/
 /*global Flickity*/
-$('#menu_mob').on('click', function(){
-    $('#menu_mob').remove();
-    $('#menu_mob').style['display'] = 'block';
-    $('nav').style['display'] = 'block';
-});
 /*SKILLS BLOCK*/
 $(".skills").addClass("active")
-$(".skills .skill .skill-bar span").each(function() {
+$(".skills .skill .skill_bar span").each(function() {
    $(this).animate({
       "width": $(this).parent().attr("data-bar") + "%"
    }, 5000);
    $(this).append('<b>' + $(this).parent().attr("data-bar") + '%</b>');
 });
 setTimeout(function() {
-   $(".skills .skill .skill-bar span b").animate({"opacity":"1"},2000);
+   $(".skills .skill .skill_bar span b").animate({"opacity":"1"},2000);
 }, 5000);
 /*SKILLS BLOCK*/
 /*TESTIMONIALS BLOCK*/
@@ -26,3 +21,53 @@ var flkty = new Flickity( '.main-gallery', {
   autoPlay: 3800
 });
 /*END TESTIMONIALS BLOCK*/
+/*MOBILE MENU*/
+$.fn.extend({
+  threeBarToggle: function(options){
+    var defaults = {
+      color: 'black',
+      width: 30,
+      height: 25,
+      speed: 400,
+      animate: true
+    }
+    var options = $.extend(defaults, options); 
+    
+    return this.each(function(){
+      
+      $(this).empty().css({'width': options.width, 'height': options.height, 'background': 'transparent'});
+      $(this).addClass('tb-menu-toggle');
+      $(this).prepend('<i></i><i></i><i></i>').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('tb-active-toggle');
+        if (options.animate) { $(this).toggleClass('tb-animate-toggle'); }
+        $('.tb-mobile-menu').slideToggle(options.speed);
+      });
+      $(this).children().css('background', options.color);
+    });
+  },
+  accordionMenu: function(options){
+    // Set the default options
+    var defaults = {
+      speed: 400
+    }
+    var options =  $.extend(defaults, options);
+
+    return this.each(function(){
+      
+      $(this).addClass('tb-mobile-menu');
+      var menuItems = $(this).children('li');
+      menuItems.find('.sub-menu').parent().addClass('tb-parent');
+      $('.tb-parent ul').hide();
+      $('.tb-parent > a').on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $(this).siblings().slideToggle(options.speed);
+      });
+      
+    });
+  }
+});
+$('#menu-toggle').threeBarToggle({color: 'white', width: 30, height: 25});
+$('#menu').accordionMenu();
+/*MOBILE MENU END*/
